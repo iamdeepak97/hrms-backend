0a79484583ec5c1ee6fb4d9cda7bdf4009dbbda1
Deepak Saral <137160350+iamdeepak97@users.noreply.github.com>
2026-03-04 02:56:45 +0530

    Change database configuration to use SQLite

--- core/settings.py
+++ core/settings.py
@@ -71,12 +71,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
